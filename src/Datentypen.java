a72045eab70d6a82a305f18af6ec09883eda1369
Kai Schell <kai.schell@oscert.eu>
2025-11-24 13:18:23 +0100

    add datentypen

---
+++ src/Datentypen.java
@@ -0,0 +1,5 @@
+public class Datentypen {
+    static void main(String[] args) {
+        System.out.println("Wir verschaffen uns einen Überblick über die verschiedenen Datentypen in Java");
+    }
+}
