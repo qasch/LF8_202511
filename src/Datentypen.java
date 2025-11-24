4829bff3f3db0b68feff36af388cbaa9d3367ff2
Kai Schell <kai.schell@oscert.eu>
2025-11-24 13:43:31 +0100

    declaration/initialization

--- src/Datentypen.java
+++ src/Datentypen.java
@@ -1,6 +1,23 @@
 public class Datentypen {
+    /*
+    Das hier ist ein mehrzeiliger Kommentar
+    alle Zeilen bis zum schliessenden Sternchen-Slash sind
+    Kommentare
+     */
     static void main(String[] args) {
         System.out.println("Wir verschaffen uns einen Überblick über die verschiedenen Datentypen in Java");
-        // das hier ist eine neue Zeile
+        // das hier ist ein einzeiliger Kommentar
+
+        // Integer - ganze Zahl
+        // Erstellen/Bekanntmachen/Deklarieren der Varriablen
+        // es gibt jetzt eine Variable mit Namen zahl, welche Integer aufnehmen kann
+        int zahl;
+        // geht noch nicht, hier gibt es die Varialbe nur, sie enthält aber noch keinen Wert
+        //System.out.println(zahl);
+
+        // Initialisierung/Wertzuweisung: der Variablen zahl wird der Wert 213 zugewiesen
+        zahl = 213;
+        // jetzt können wir uns den Wert der Variablen ausgeben lassen:
+        System.out.println(zahl);
     }
 }
