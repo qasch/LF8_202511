7cd00d43343c8387d24cb0338d68bf036ca4ded0
Kai Schell <kai.schell@oscert.eu>
2025-11-24 14:29:00 +0100

    edit datentypen

--- src/Datentypen.java
+++ src/Datentypen.java
@@ -19,5 +19,27 @@ public class Datentypen {
         zahl = 213;
         // jetzt können wir uns den Wert der Variablen ausgeben lassen:
         System.out.println(zahl);
+        System.out.println("Der Inhalt der Variablen zahl ist " + zahl + " (eine ganze Zahl)");   // + -> Konkatenationsoperator
+
+        // Deklaration und Initialiserung in einem Schritt:
+        double gleitKommaZahlDouble = 2.4;
+        float gleitKommaZahlFloat = 3.14f;
+
+        System.out.println(gleitKommaZahlDouble);
+
+        // Wahrheitswert: True / False
+        boolean jaNein = true;
+        jaNein = false;
+
+        // einzelne Buchstaben bzw. Zeichen (Character) müssen in einfache Hochkommata eingefasst werden
+        // im Gegensatz zu Strings, die in doppelten stehen müssen
+        char einzelnesZeichen = 'a';
+
+        String zeichenkette = "Ich bin eine Zeichenkette";
+        System.out.println(zeichenkette);
+
+        System.out.println(zeichenkette.toUpperCase());
+
+
     }
 }
