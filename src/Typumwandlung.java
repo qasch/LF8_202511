6a43cfd1d3b399a21a1c5cd545ff4462807803bf
Kai Schell <kai.schell@oscert.eu>
2025-11-25 11:59:20 +0100

    edit typumwandlung

--- src/Typumwandlung.java
+++ src/Typumwandlung.java
@@ -6,10 +6,28 @@ public class Typumwandlung {
         int intZahl = (int) doubleZahl;   // explizites Casting
         System.out.println(intZahl);
 
-        doubleZahl = (double) intZahl;   // explizites Casting
+        doubleZahl = intZahl;   // implizites Casting
         System.out.println(doubleZahl);
 
         char text = 'c';
-        intZahl = (int) text;
+        intZahl = text;   // implizites Casting
+
+
+       // Datentyp byte
+       // 1 Byte -> 8 Bit
+       // 0 bis 255
+       // -128 bis 127
+       byte kleineZahl = 127;
+        System.out.println(kleineZahl);
+        // kleineZahl = kleineZahl + 1
+        // kleineZahl = 127        + 1   -> 128
+        kleineZahl++;
+        System.out.println(kleineZahl);
+        kleineZahl++;
+        System.out.println(kleineZahl);
+
+
+
+
     }
 }
