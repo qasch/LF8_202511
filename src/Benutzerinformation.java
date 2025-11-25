c285623d3785619edb6a6bb4e5823f82628807a4
Kai Schell <kai.schell@oscert.eu>
2025-11-25 09:36:50 +0100

    add examples

--- src/Benutzerinformation.java
+++ src/Benutzerinformation.java
@@ -1,2 +1,23 @@
 public class Benutzerinformation {
+    public static void main(String[] args) {
+        // Zeichenkette
+        String name = "* G r e t l *";  // Leer- und Sonderzeichen sind erlaubt
+        // Ganze Zahl
+        int alter = 12;
+        // Gleitkommazahl
+        double groesse = 0.32;   // Punkt ist das Dezimaltrennzeichen, nicht das Komma
+        // Wahrheitswert
+        boolean schueler = false;
+
+        // Ausgabe auf der Konsole
+        System.out.print(name + "\n");  // name ist ein String
+        System.out.println(alter);      // implizites Casting von int zu String
+        System.out.println(
+                // wir können unsere Ausgabe auch hier schon formatieren
+                // in Java sind Zeilenumbrüche mehr oder weniger egal,
+                // anders als in Python
+                "Größe:\t\t" + groesse + "m" + "" +     // + ist der Konkatenationsoperator, der zwei Strings aneinanderklebt
+                "\nSchüler:\t" + schueler               // \n macht einen Zeilenumbruch, \t ein Tab
+        );  // erst hier wird unsere Zeile durch ein Semikolon beendet
+    }
 }
--- src/Typumwandlung.java
+++ src/Typumwandlung.java
@@ -1,2 +1,15 @@
 public class Typumwandlung {
+    public static void main(String[] args) {
+        double doubleZahl = 45.78;
+        System.out.println(doubleZahl);
+
+        int intZahl = (int) doubleZahl;   // explizites Casting
+        System.out.println(intZahl);
+
+        doubleZahl = (double) intZahl;   // explizites Casting
+        System.out.println(doubleZahl);
+
+        char text = 'c';
+        intZahl = (int) text;
+    }
 }
