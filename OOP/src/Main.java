55d235dd5766dcae13a098854226a21c8f01c15a
Kai Schell <kai.schell@oscert.eu>
2025-11-27 14:39:46 +0100

    add oop

--- OOP/src/Main.java
+++ OOP/src/Main.java
@@ -1,2 +1,29 @@
 public class Main {
+    public static void main(String[] args) {
+
+        // Aufruf Konstruktor mit vier Parametern
+        Tier hund = new Tier(3, "Hänsel", "Hund", 3.7);
+        hund.beschreibeDichSelbst();
+        hund.friss();
+        hund.gibLaut("wuff wuff");
+
+        // Aufruf Konstruktor mit vier Parametern
+        Tier vogel = new Tier(2, "Berta Vogel", "Vogel", 10.2);
+        vogel.beschreibeDichSelbst();
+        vogel.gibLaut("piep piep");
+
+        // Aufruf Konstruktor mit vier Parametern
+        Tier fisch = new Tier(0, "Nemo", "Fisch", 0.7);
+        fisch.beschreibeDichSelbst();
+        fisch.gibLaut("blub blub");
+
+        // Aufruf Standarkonstruktor
+        Tier tier = new Tier();
+        tier.beschreibeDichSelbst();
+
+        // Aufruf Konstruktor mit zwei Parametern
+        Tier katze = new Tier("Jumbo", "Katze");
+        katze.beschreibeDichSelbst();
+        katze.gibLaut("miau miau");
+    }
 }
--- OOP/src/Tier.java
+++ OOP/src/Tier.java
@@ -1,2 +1,59 @@
 public class Tier {
+    // Eigenschaften/Attribute
+    int anzahlBeine;
+    String name;
+    String art;
+    double geschwindigkeit;
+
+    // Konstruktor - soll ein neu erzeugtes Objekt in einen sinnvollen
+    // Ausgangszustand versetzen (mit sinnvollen Initialwerten versehen)
+
+    // Standardkonstruktor / parameterlose Konstruktor
+    // solange kein eigener Konstruktor definiert ist, wir dieser aufgerufen
+    // falls wir aber einen eigenen Konstruktor erstellt haben, müssten wir
+    // ihn wieder hinschreiben
+    public Tier(){
+
+    }
+
+    // Konstruktor mit zwei Parametern (überschreibt den Standardkonstruktor -> Polymorphie)
+    public Tier(String name, String art) {
+        this.anzahlBeine = 4;
+        this.name = name;
+        this.art = art;
+    }
+
+    // Konstruktor mit vier Parametern (überschreibt den Standardkonstruktor -> Polymorphie)
+    public Tier(int anzahlBeine, String name, String art, double geschwindigkeit) {
+        this.anzahlBeine = anzahlBeine;
+        this.name = name;
+        this.art = art;
+        this.geschwindigkeit = geschwindigkeit;
+    }
+
+    // Fähigkeiten/Funktionalitäten
+    void friss(){
+        System.out.println("mampf mampf");
+    }
+
+    void gibLaut(String laut){
+        System.out.println(laut);
+    }
+
+    void beschreibeDichSelbst(){
+        System.out.println();
+        System.out.println("------- " + this.name + " -------");
+
+        System.out.print("Wie viele Beine hat unser Tier? ");
+        System.out.println(this.anzahlBeine);
+
+        System.out.print("Wie schnell ist unser Tier? ");
+        System.out.println(this.geschwindigkeit);
+
+        System.out.print("Wie heißt unser Tier? ");
+        System.out.println(this.name);
+
+        System.out.print("Welche Art ist unser Tier? ");
+        System.out.println(this.art);
+    }
 }
