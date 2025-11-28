00d27bb6690a69de5e2e1dc5a9d5ee40ea12805f
Kai Schell <kai.schell@oscert.eu>
2025-11-28 16:13:03 +0100

    clean up again

--- OOP/src/Flugfaehig.java
+++
@@ -1,8 +0,0 @@
-/**
- * Interface beschreibt nur, was getan werden kann (kein Zustand, keine Umsetzung).
- */
-public interface Flugfaehig {
-    void starte();
-    void fliege();
-    void lande();
-}
--- OOP/src/Mensch.java
+++
@@ -1,5 +0,0 @@
-public class Mensch {
-    String name;
-
-
-}
